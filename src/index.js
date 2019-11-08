a06f5fd38e6a814da020b41b8a905a9a81fc20ed
marzzy mogh <marzzy.m@gmail.com>
2019-11-08 10:12:38 +0330

    chore: ready project to add router

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
-import './js/containers/FormContainer';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import FormContainer from './js/containers/App';
+
+const wrapper = document.getElementById('root');
+ReactDOM.render(<FormContainer />, wrapper);
---
+++ src/js/containers/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      hiiiiiiiiiiiiii
+    </div>
+  );
+}
+
+export default App;
